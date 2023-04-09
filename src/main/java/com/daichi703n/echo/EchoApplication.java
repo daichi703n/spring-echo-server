334a058cc2a49ada538e440767cae3184a5c0ee4
Daichi Kimura <daichi703n@gmail.com>
2023-04-10 00:02:06 +0900

    Initial Commit

---
+++ src/main/java/com/daichi703n/echo/EchoApplication.java
@@ -0,0 +1,13 @@
+package com.daichi703n.echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoApplication.class, args);
+	}
+
+}
